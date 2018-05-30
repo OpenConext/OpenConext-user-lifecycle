269d4ce1b6165c632d1582eb23adfb4dd9091662
Michiel Kodde <mkodde@ibuildings.nl>
2018-05-30 09:55:36 +0200

    Implement the batchDeprovision method
    The method was implemented on the DeprovisionService.

--- src/OpenConext/UserLifecycle/Application/Service/DeprovisionService.php
+++ src/OpenConext/UserLifecycle/Application/Service/DeprovisionService.php
@@ -18,11 +18,11 @@
 
 namespace OpenConext\UserLifecycle\Application\Service;
 
-use InvalidArgumentException;
+use OpenConext\UserLifecycle\Domain\Client\BatchInformationResponseCollection;
 use OpenConext\UserLifecycle\Domain\Client\DeprovisionClientCollectionInterface;
-use OpenConext\UserLifecycle\Domain\Client\InformationResponseInterface;
 use OpenConext\UserLifecycle\Domain\Service\DeprovisionServiceInterface;
-use OpenConext\UserLifecycle\Domain\Service\InformationServiceInterface;
+use OpenConext\UserLifecycle\Domain\Service\LastLoginServiceInterface;
+use OpenConext\UserLifecycle\Domain\Service\SanityCheckServiceInterface;
 use OpenConext\UserLifecycle\Domain\ValueObject\CollabPersonId;
 use Psr\Log\LoggerInterface;
 use Webmozart\Assert\Assert;
@@ -35,30 +35,42 @@ class DeprovisionService implements DeprovisionServiceInterface
     private $deprovisionClientCollection;
 
     /**
+     * @var SanityCheckServiceInterface
+     */
+    private $sanityCheckService;
+
+    /**
+     * @var LastLoginServiceInterface
+     */
+    private $lastLoginService;
+
+    /**
      * @var LoggerInterface
      */
     private $logger;
 
     public function __construct(
         DeprovisionClientCollectionInterface $deprovisionClientCollection,
+        SanityCheckServiceInterface $sanityCheckService,
+        LastLoginServiceInterface $lastLoginService,
         LoggerInterface $logger
     ) {
         $this->deprovisionClientCollection = $deprovisionClientCollection;
+        $this->sanityCheckService = $sanityCheckService;
+        $this->lastLoginService = $lastLoginService;
         $this->logger = $logger;
     }
 
     /**
      * @param string $personId
      * @param bool $dryRun
-     * @return InformationResponseInterface
+     * @return string
      */
     public function deprovision($personId, $dryRun = false)
     {
         $this->logger->debug('Received a request to deprovision a user.');
 
-        Assert::stringNotEmpty($personId, 'Please pass a non empty collabPersonId');
-
-        $collabPersonId = new CollabPersonId($personId);
+        $collabPersonId = $this->buildCollabPersonId($personId);
 
         $this->logger->debug('Delegate deprovisioning to the registered services.');
 
@@ -70,5 +82,44 @@ class DeprovisionService implements DeprovisionServiceInterface
         );
 
         return $information;
+    }
+
+    /**
+     * Finds the users marked for deprovisioning, and deprovisions them.
+     *
+     * @param bool $dryRun
+     * @return string
+     */
+    public function batchDeprovision($dryRun = false)
+    {
+        $this->logger->debug('Retrieve the users that are marked for deprovisioning.');
+        $users = $this->lastLoginService->findUsersForDeprovision();
+
+        $this->logger->debug('Perform sanity checks on the response from the last login service.');
+        $this->sanityCheckService->check($users);
+
+        $batchInformationCollection = new BatchInformationResponseCollection();
+        foreach ($users->getData() as $lastLogin) {
+            $collabPersonId = $this->buildCollabPersonId($lastLogin->getCollabPersonId());
+            $information = $this->deprovisionClientCollection->deprovision($collabPersonId, $dryRun);
+            $batchInformationCollection->add($collabPersonId, $information);
+
+            $this->logger->info(
+                sprintf(
+                    'Received deprovision information for user "%s" with the following data.',
+                    $lastLogin->getCollabPersonId()
+                ),
+                ['information_response' => $information]
+            );
+        }
+
+        return $batchInformationCollection->jsonSerialize();
+    }
+
+    private function buildCollabPersonId($personId)
+    {
+        Assert::stringNotEmpty($personId, 'Please pass a non empty collabPersonId');
+
+        return new CollabPersonId($personId);
     }
 }
--- tests/unit/Application/Service/DeprovisionServiceTest.php
+++ tests/unit/Application/Service/DeprovisionServiceTest.php
@@ -23,6 +23,10 @@ use Mockery as m;
 use Mockery\Mock;
 use OpenConext\UserLifecycle\Application\Service\DeprovisionService;
 use OpenConext\UserLifecycle\Domain\Client\InformationResponseCollectionInterface;
+use OpenConext\UserLifecycle\Domain\Collection\LastLoginCollectionInterface;
+use OpenConext\UserLifecycle\Domain\Entity\LastLogin;
+use OpenConext\UserLifecycle\Domain\Service\LastLoginServiceInterface;
+use OpenConext\UserLifecycle\Domain\Service\SanityCheckServiceInterface;
 use OpenConext\UserLifecycle\Infrastructure\UserLifecycleBundle\Client\DeprovisionClientCollection;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
@@ -39,11 +43,28 @@ class DeprovisionServiceTest extends TestCase
      */
     private $apiCollection;
 
+    /**
+     * @var SanityCheckServiceInterface|Mock
+     */
+    private $sanityChecker;
+
+    /**
+     * @var LastLoginServiceInterface|Mock
+     */
+    private $lastLoginService;
+
     public function setUp()
     {
         $this->apiCollection = m::mock(DeprovisionClientCollection::class);
+        $this->sanityChecker = m::mock(SanityCheckServiceInterface::class);
+        $this->lastLoginService = m::mock(LastLoginServiceInterface::class);
         $logger = m::mock(LoggerInterface::class)->shouldIgnoreMissing();
-        $this->service = new DeprovisionService($this->apiCollection, $logger);
+        $this->service = new DeprovisionService(
+            $this->apiCollection,
+            $this->sanityChecker,
+            $this->lastLoginService,
+            $logger
+        );
     }
 
     public function test_deprovision()
@@ -57,11 +78,14 @@ class DeprovisionServiceTest extends TestCase
 
         $this->apiCollection
             ->shouldReceive('deprovision')
-            ->andReturnUsing(function ($expectedCollabPersonId, $expectedDryRunState) use ($collection) {
-                $this->assertEquals('jay-leno', $expectedCollabPersonId->getCollabPersonId());
-                $this->assertFalse($expectedDryRunState);
-                return $collection;
-            });
+            ->andReturnUsing(
+                function ($expectedCollabPersonId, $expectedDryRunState) use ($collection) {
+                    $this->assertEquals('jay-leno', $expectedCollabPersonId->getCollabPersonId());
+                    $this->assertFalse($expectedDryRunState);
+
+                    return $collection;
+                }
+            );
 
         // Call the readInformationFor method
         $response = $this->service->deprovision($personId);
@@ -101,4 +125,93 @@ class DeprovisionServiceTest extends TestCase
 
         $this->service->deprovision($personId);
     }
+
+    public function test_batch_deprovision()
+    {
+        $mockCollection = m::mock(LastLoginCollectionInterface::class);
+        $mockUser1 = $this->buildMockLastLoginEntry('jack-black');
+        $mockUser2 = $this->buildMockLastLoginEntry('jan-berry');
+
+        $this->lastLoginService
+            ->shouldReceive('findUsersForDeprovision')
+            ->andReturn($mockCollection);
+
+        $this->sanityChecker
+            ->shouldReceive('check')
+            ->with($mockCollection)
+            ->andReturnNull();
+
+        $mockCollection
+            ->shouldReceive('getData')
+            ->andReturn([$mockUser1, $mockUser2]);
+
+        $collection = m::mock(InformationResponseCollectionInterface::class);
+        $collection
+            ->shouldReceive('jsonSerialize')
+            ->andReturn('{"only": "test"}');
+
+        $this->apiCollection
+            ->shouldReceive('deprovision')
+            ->andReturnUsing(
+                function ($expectedCollabPersonId, $expectedDryRunState) use ($mockCollection, $collection) {
+                    $this->assertContains($expectedCollabPersonId->getCollabPersonId(), ['jack-black', 'jan-berry']);
+                    $this->assertFalse($expectedDryRunState);
+
+                    return $collection;
+                }
+            );
+
+        $this->service->batchDeprovision();
+
+    }
+
+    public function test_batch_deprovision_dry_run()
+    {
+        $mockCollection = m::mock(LastLoginCollectionInterface::class);
+        $mockUser1 = $this->buildMockLastLoginEntry('jack-black');
+        $mockUser2 = $this->buildMockLastLoginEntry('jan-berry');
+
+        $this->lastLoginService
+            ->shouldReceive('findUsersForDeprovision')
+            ->andReturn($mockCollection);
+
+        $this->sanityChecker
+            ->shouldReceive('check')
+            ->with($mockCollection)
+            ->andReturnNull();
+
+        $mockCollection
+            ->shouldReceive('getData')
+            ->andReturn([$mockUser1, $mockUser2]);
+
+        $collection = m::mock(InformationResponseCollectionInterface::class);
+        $collection
+            ->shouldReceive('jsonSerialize')
+            ->andReturn('{"only": "test"}');
+
+        $this->apiCollection
+            ->shouldReceive('deprovision')
+            ->andReturnUsing(
+                function ($expectedCollabPersonId, $expectedDryRunState) use ($mockCollection, $collection) {
+                    $this->assertContains($expectedCollabPersonId->getCollabPersonId(), ['jack-black', 'jan-berry']);
+                    $this->assertTrue($expectedDryRunState);
+
+                    return $collection;
+                }
+            );
+
+        $this->service->batchDeprovision(true);
+
+    }
+
+    private function buildMockLastLoginEntry($personId)
+    {
+        $lastLogin = m::mock(LastLogin::class);
+        $lastLogin
+            ->shouldReceive('getCollabPersonId')
+            ->andReturn($personId);
+
+        return $lastLogin;
+    }
+
 }
