d1f69da4edfc9c0739dfa09375515c0689787e94
Michiel Kodde <mkodde@ibuildings.nl>
2018-07-03 08:37:32 +0200

    Create the feature toggle for deprovision Api
    In config we can enable/disable the deprovision Api. The config setting
    is reflected in a feature toggle that defaults to false.

--- src/OpenConext/UserLifecycle/Infrastructure/UserLifecycleBundle/Controller/ApiController.php
+++ src/OpenConext/UserLifecycle/Infrastructure/UserLifecycleBundle/Controller/ApiController.php
@@ -2,10 +2,25 @@
 
 namespace OpenConext\UserLifecycle\Infrastructure\UserLifecycleBundle\Controller;
 
+use OpenConext\UserLifecycle\Application\Api\DeprovisionApiFeatureToggle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ApiController extends Controller
 {
+    /**
+     * @var DeprovisionApiFeatureToggle
+     */
+    private $enabled;
+
+    /**
+     * @param DeprovisionApiFeatureToggle $isEnabled
+     */
+    public function __construct(DeprovisionApiFeatureToggle $isEnabled)
+    {
+        $this->enabled = $isEnabled;
+        die(var_dump($isEnabled));
+    }
+
     public function deprovisionAction()
     {
         return $this->json([]);
