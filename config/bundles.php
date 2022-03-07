3a07aec1569e690568dde0893012a71ae9891c63
Michiel Kodde <mkodde@ibuildings.nl>
2022-03-07 09:49:10 +0100

    Upgrade to Symfony 5.4
    This only entailed getting a working set op packages. Warnings, errors,
    deprecations are yet to be addressed.

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     OpenConext\MonitorBundle\OpenConextMonitorBundle::class => ['all' => true],
