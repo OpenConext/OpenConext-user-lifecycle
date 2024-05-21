8a23843b28049ff46a3acdfda9434836fd5a617a
Paul Rijke <paul@ibuildings.nl>
2024-05-21 13:14:27 +0200

    Sort bundles alphabetically

--- config/bundles.php
+++ config/bundles.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    OpenConext\MonitorBundle\OpenConextMonitorBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    OpenConext\UserLifecycle\Infrastructure\UserLifecycleBundle\UserLifecycleBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    OpenConext\MonitorBundle\OpenConextMonitorBundle::class => ['all' => true],
+    OpenConext\UserLifecycle\Infrastructure\UserLifecycleBundle\UserLifecycleBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
